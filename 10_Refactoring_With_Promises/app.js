0705cd132ba9b7cafa621f73d660a7a0f97f4fe7
krushangi <kshingala@argusoft.com>
2023-03-17 17:57:22 +0530

    async await  add

--- 10_Refactoring_With_Promises/app.js
+++ 10_Refactoring_With_Promises/app.js
@@ -16,104 +16,82 @@ const moveX = (element, amount, delay) => {
 };
 
 const btn = document.querySelector('button');
-moveX(btn, 100, 1000)
-	.then(() => moveX(btn, 100, 1000))
-	.then(() => moveX(btn, 100, 1000))
-	.then(() => moveX(btn, 100, 1000))
-	.then(() => moveX(btn, 100, 1000))
-	.then(() => moveX(btn, 100, 1000))
-	.then(() => moveX(btn, 100, 1000))
-	.then(() => moveX(btn, 100, 1000))
-	.then(() => moveX(btn, 100, 1000))
-	.then(() => moveX(btn, 100, 1000))
-	.then(() => moveX(btn, 100, 1000))
-	.then(() => moveX(btn, 100, 1000))
-	.then(() => moveX(btn, 100, 1000))
-	.then(() => moveX(btn, 100, 1000))
-	.then(() => moveX(btn, 100, 1000))
-	.then(() => moveX(btn, 100, 1000))
-	.catch(({ bodyBoundary, amount, elRight }) => {
-		console.log(`Cannot Move! Body is ${bodyBoundary}px wide`);
-		console.log(`Element is at ${elRight}px, ${amount}px is too large!`);
-	});
+// moveX(btn, 100, 1000)
+// 	.then(() => moveX(btn, 100, 1000))
+// 	.then(() => moveX(btn, 100, 1000))
+// 	.then(() => moveX(btn, 100, 1000))
+// 	.then(() => moveX(btn, 100, 1000))
+// 	.then(() => moveX(btn, 100, 1000))
+// 	.then(() => moveX(btn, 100, 1000))
+// 	.then(() => moveX(btn, 100, 1000))
+// 	.then(() => moveX(btn, 100, 1000))
+// 	.then(() => moveX(btn, 100, 1000))
+// 	.then(() => moveX(btn, 100, 1000))
+// 	.then(() => moveX(btn, 100, 1000))
+// 	.then(() => moveX(btn, 100, 1000))
+// 	.then(() => moveX(btn, 100, 1000))
+// 	.then(() => moveX(btn, 100, 1000))
+// 	.then(() => moveX(btn, 100, 1000))
+// 	.catch(({ bodyBoundary, amount, elRight }) => {
+// 		console.log(`Cannot Move! Body is ${bodyBoundary}px wide`);
+// 		console.log(`Element is at ${elRight}px, ${amount}px is too large!`);
+// 	});
 
-//This function moves an element "amount" number of pixels after a delay.
-//If the element will stay on screen, we move the element and call the onSuccess callback function
-//If the element will move off screen, we do not move the element and instead call the onFailure callback
-// const moveX = (element, amount, delay, onSuccess, onFailure) => {
-// 	setTimeout(() => {
-// 		const bodyBoundary = document.body.clientWidth;
-// 		const elRight = element.getBoundingClientRect().right;
-// 		const currLeft = element.getBoundingClientRect().left;
-// 		if (elRight + amount > bodyBoundary) {
-// 			onFailure();
-// 		}
-// 		else {
-// 			element.style.transform = `translateX(${currLeft + amount}px)`;
-// 			onSuccess();
-// 		}
-// 	}, delay);
-// };
 
-// LOOK AT THIS UGLY MESS!
-// moveX(
-// 	btn,
-// 	300,
-// 	1000,
-// 	() => {
-// 		//success callback
-// 		moveX(
-// 			btn,
-// 			300,
-// 			1000,
-// 			() => {
-// 				//success callback
-// 				moveX(
-// 					btn,
-// 					300,
-// 					1000,
-// 					() => {
-// 						//success callback
-// 						moveX(
-// 							btn,
-// 							300,
-// 							1000,
-// 							() => {
-// 								//success callback
-// 								moveX(
-// 									btn,
-// 									300,
-// 									1000,
-// 									() => {
-// 										//success callback
-// 										alert('YOU HAVE A WIDE SCREEN!');
-// 									},
-// 									() => {
-// 										//failure callback
-// 										alert('CANNOT MOVE FURTHER!');
-// 									}
-// 								);
-// 							},
-// 							() => {
-// 								//failure callback
-// 								alert('CANNOT MOVE FURTHER!');
-// 							}
-// 						);
-// 					},
-// 					() => {
-// 						//failure callback
-// 						alert('CANNOT MOVE FURTHER!');
-// 					}
-// 				);
-// 			},
-// 			() => {
-// 				//failure callback
-// 				alert('CANNOT MOVE FURTHER!');
-// 			}
-// 		);
-// 	},
-// 	() => {
-// 		//failure callback
-// 		alert('CANNOT MOVE FURTHER!');
-// 	}
-// );
+	//async and await
+	//async return promise
+	//await run after promise resolve
+
+	let s;
+	async function sum(x,y){
+		 s=x+y;
+		return s;
+	}
+
+	sum().then( (data) =>{
+		console.log(`sum is ${data}`);
+	})
+
+
+	async function getData (){
+		const d= await axios.get('https://github.com/krushangi1/Js:/8080');
+		console.log(d.data);
+	}
+
+	getData().catch( (err)=>
+	{
+		console.log('error catch');
+		console.log(err);
+	} );
+
+
+//moveX using async and await
+	async function newMove(el,amt){
+		await moveX(el,amt,1000);
+		await moveX(el,amt,1000);
+		await moveX(el,amt,1000);
+		await moveX(el,amt,1000);
+		await moveX(el,amt,1000);
+		await moveX(el,amt,1000);
+		await moveX(el,amt,1000);
+		await moveX(el,amt,1000);
+		await moveX(el,amt,1000);
+	}
+
+	// newMove(btn).catch( (err)=>{
+	// 	console.log("DONE!!!");
+	// } );
+
+		newMove(btn,100).catch( (err)=>{
+			console.log("DONE!!!");
+			newMove(btn,-100);
+		} );
+
+		//PROTYPE
+
+		let arr=["kru","ros"];
+		let obj={
+			name:"ban",
+			city:"ahm"
+		}
+		arr.__proto__=obj;
